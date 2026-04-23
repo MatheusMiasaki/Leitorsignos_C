4bff5f7f6109a1eb95c353e9c3d468c6446052f8
Matheus Miasaki Sanga <matheusmiasakisanga@gmail.com>
2026-04-23 11:14:30 -0300

    04

--- lib.c
+++ lib.c
@@ -3,9 +3,21 @@
 
 // função para determinar signos e abrir o arquivo HTML
 int determinar_signo(int dia, int mes) {
+      // Mês tem que ser maior q 1 e menor q 12 || Dia tem que ser maior q 1 e menor q 12
+    if (mes < 1 || mes > 12 || dia < 1 || dia > 31) {
+        return printf("Data inválida, DIGITE NOVAMENTE!!\n");
+    }
+
+    // Validação de mês de acordo com os dias mes 2>29 dias 4,6,9,11 meses com 30 dias
+    if ((mes == 2 && dia > 29) ||
+        ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30)) {
+        return printf("Data inválida, DIGITE NOVAMENTE!!\n");
+    }
+
     // Áries: 21/03 a 19/04
     if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/aries.html");
+        //return system("xdg-open /home/aluno/Documentos/trabalhoc/html/aries.html");
+        return printf ("Áries");
     }
     // Touro: 20/04 a 20/05
     else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20)) {
@@ -50,9 +62,7 @@ int determinar_signo(int dia, int mes) {
     // Peixes: 19/02 a 20/03
     else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20)) {
         return system("xdg-open /home/aluno/Documentos/trabalhoc/html/peixes.html");
-    }
-
-    return -1; // Caso a data seja inválida
+    } return (0);
 }
 
 // no mes 1 tem que ser de 21 a 31 e no mes 2 ate dia 19 
--- main.c
+++ main.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include "lib.c"
 
-int main(){
+int main() {
     int dia, mes;
-    printf("Digite o dia do seu aniversário:");
-    scanf ("%d",&dia);
-    printf("Digite o mês do seu aniversário:");
-    scanf ("%d",&mes);
+    do {
+        printf("Digite o dia do seu aniversário:\n ");
+        scanf("%d", &dia);
+        printf("Digite o mês do seu aniversário (0 para sair):\n ");
+        scanf("%d", &mes);
 
-    dia, mes = determinar_signo(dia,mes);
+        if (mes == 0) {
+            break;
+        }
+        determinar_signo(dia, mes);
+    } while (1);
 
-    
-
-    
-
-    
-
-
+    return 0;
 }
