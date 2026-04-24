e1b6716e45d9aa1826927710431e50a66732edd8
MatheusMiasaki <matheusmiasakisanga@gmail.com>
2026-04-24 19:46:14 -0300

    FinalVersion

--- lib.c
+++ lib.c
@@ -14,60 +14,6 @@ int determinar_signo(int dia, int mes) {
         return printf("Data inválida, DIGITE NOVAMENTE!!\n");
     }
 
-<<<<<<< HEAD
-    // Áries: 21/03 a 19/04
-    if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19)) {
-        //return system("xdg-open /home/aluno/Documentos/trabalhoc/html/aries.html");
-        return printf ("Áries");
-    }
-    // Touro: 20/04 a 20/05
-    else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/touro.html");
-    }
-    // Gêmeos: 21/05 a 20/06
-    else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/gemeos.html");
-    }
-    // Câncer: 21/06 a 22/07
-    else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/cancer.html");
-    }
-    // Leão: 23/07 a 22/08
-    else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/leao.html");
-    }
-    // Virgem: 23/08 a 22/09
-    else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/virgem.html");
-    }
-    // Libra: 23/09 a 22/10
-    else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/libra.html");
-    }
-    // Escorpião: 23/10 a 21/11
-    else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/escorpiao.html");
-    }
-    // Sagitário: 22/11 a 21/12
-    else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/sagitario.html");
-    }
-    // Capricórnio: 22/12 a 19/01
-    else if ((mes == 12 && dia >= 22) || (mes == 1 && dia <= 19)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/capricornio.html");
-    }
-    // Aquário: 20/01 a 18/02
-    else if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/aquario.html");
-    }
-    // Peixes: 19/02 a 20/03
-    else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20)) {
-        return system("xdg-open /home/aluno/Documentos/trabalhoc/html/peixes.html");
-    } return (0);
-}
-
-// no mes 1 tem que ser de 21 a 31 e no mes 2 ate dia 19 
-=======
     // Áries: 21/03 a 20/04
     if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 20)) {
         return system("xdg-open ../html/aries.html");
@@ -117,4 +63,4 @@ int determinar_signo(int dia, int mes) {
         return system("xdg-open ../html/peixes.html");
     } return (0);
 }
->>>>>>> 5311930 (Final)
+
--- main.c
+++ main.c
@@ -3,17 +3,18 @@
 
 int main() {
     int dia, mes;
+    
     do {
         printf("Digite o dia do seu aniversário:\n ");
         scanf("%d", &dia);
         printf("Digite o mês do seu aniversário (0 para sair):\n ");
         scanf("%d", &mes);
-
+        //Se usuário digitar 0 ele quebra a estrutura de repetição
         if (mes == 0) {
             break;
         }
         determinar_signo(dia, mes);
-    } while (1);
+    } while (1); // Repetir até que o usuário escolha sair
 
     return 0;
 }
